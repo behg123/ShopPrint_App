ae36132f2dfecbeda0f92c2c0c2073cb1d67cf48
behg <bernardo.gaya2@gmail.com>
2023-04-26 19:22:04 -0300

    Star projec

---
+++ ShopPrint_App/ShopPrint_App/ShopPrint_App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ShopPrint_App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ShopPrint_App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
